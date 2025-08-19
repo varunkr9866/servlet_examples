f13a9e51e3040dca6b4731fcda370d6e1fd783ad
varun <varunkr9866@gmail.com>
2025-08-19 16:25:00 +0530

    Servlet Program Completed

--- 02simpleapp/src/main/java/com/cluster/LoginServlet.java
+++ 02simpleapp/src/main/java/com/cluster/LoginServlet.java
@@ -1,5 +1,34 @@
 package com.cluster;
 
-public class Simpleapp02 {
-	
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		res.setContentType("text/html");
+		PrintWriter pw = res.getWriter();
+
+		String strNme = req.getParameter("nme");
+		String strPwd = req.getParameter("pwd");
+
+		if (strNme.equalsIgnoreCase("ravi") && strPwd.equalsIgnoreCase("amog")) {
+			pw.println("<html>");
+			pw.println("<body bgcolor='pink'>");
+			pw.println("Hello MR." + strNme + "You Are Welcome");
+			pw.println("</body>");
+			pw.println("</html>");
+
+		} else {
+			pw.println("<html>");
+			pw.println("<body bgcolor='pink'>");
+			pw.println("Hello MR." + strNme + "You Are Invalid User");
+			pw.println("</body>");
+			pw.println("</html>");
+		}
+	}
 }
