e8879838c9ba31490e91d9cdfd4fc4b0a7bb34c3
varun <varunkr9866@gmail.com>
2025-08-21 14:40:56 +0530

    COMPLETED

--- 08servletconfigapp/src/main/java/com/cluster/ConfigCustomerServlet.java
+++ 08servletconfigapp/src/main/java/com/cluster/ConfigCustomerServlet.java
@@ -8,55 +8,68 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class ConfigCustomerServlet {
-		Connection con = null;
-		public void init(ServletConfig config){
-			String StrDriver = config.getInitParameter("driver");
-			String StrUrl = config.getInitParameter("dburl");
-			String StrUn = config.getInitParameter("dbuid");
-			String StrPwd = config.getInitParameter("dbpwd");
-			try {
-				Class.forName("StrDriver");
-				con =DriverManager.getConnection(StrUrl, StrUn, StrPwd);
-			} catch (ClassNotFoundException | SQLException e) {
-				System.out.println("Exception Caught :"+ e);
-				e.printStackTrace();
-			}
-		}
-		
-		public void service(HttpServletRequest req ,HttpServletResponse res) {
-			
-			String strName = req.getParameter("nme"); 
-			String strPwd = req.getParameter("pwd"); 
-			String strEmail = req.getParameter("email"); 
-			String strPhone = req.getParameter("phone"); 
-			res.setContentType("text/html");
-			PreparedStatement pst = null;
-			
-			
-			try {
-				PrintWriter pw = res.getWriter();
-				int i= 0;
-				pst = con.prepareStatement("INSERT INTO CUSTOMER (CUS_NAME,CUS_PASSWORD,CUS_EMAIL,CUS_PHONE) VALUES(?,?,?,?)");
-				
-				pst.setString(1, strName);
-				pst.setString(1, strPwd);
-				pst.setString(1, strEmail);
-				pst.setString(1, strPhone);
-				i =pst.executeUpdate();
-				
-				if (i == 1) {
-					
-				} else {
+public class ConfigCustomerServlet extends HttpServlet {
+	Connection con = null;
 
-				}
-			} catch (SQLException | IOException e) {
-				System.out.println("Exception Caught :"+ e);
-				e.printStackTrace();
-			}
-			
+	public void init(ServletConfig config) throws ServletException {
+		String StrDriver = config.getInitParameter("driver");
+		String StrUrl = config.getInitParameter("dburl");
+		String StrUn = config.getInitParameter("dbuid");
+		String StrPwd = config.getInitParameter("dbpwd");
+		try {
+			Class.forName(StrDriver);
+			con = DriverManager.getConnection(StrUrl, StrUn, StrPwd);
+		} catch (ClassNotFoundException | SQLException e) {
+			System.out.println("Exception Caught :" + e);
+			e.printStackTrace();
 		}
+	}
+
+	public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+		String strName = req.getParameter("nme");
+		String strPwd = req.getParameter("pwd");
+		String strEmail = req.getParameter("email");
+		String strPhone = req.getParameter("phone");
+		res.setContentType("text/html");
+		PreparedStatement pst = null;
+
+		try {
+			PrintWriter pw = res.getWriter();
+			int i = 0;
+			pst = con.prepareStatement(
+					"INSERT INTO CUSTOMER (CUS_NAME,CUS_PASSWORD,CUS_EMAIL,CUS_PHONE) VALUES(?,?,?,?)");
+
+			pst.setString(1, strName);
+			pst.setString(1, strPwd);
+			pst.setString(1, strEmail);
+			pst.setString(1, strPhone);
+			i = pst.executeUpdate();
+
+			if (i == 1) {
+				pw.println("<html>");
+				pw.println("<body bgcolor='green'>");
+				pw.println("Hello Mr " + strName + " Thank you for becoming a member");
+				pw.println("</body>");
+				pw.println("</html>");
+			}
+
+			else {
+				pw.println("<html>");
+				pw.println("<body bgcolor='red'>");
+				pw.println("Hello Mr " + strName + " Better luck next time");
+				pw.println("</body>");
+				pw.println("</html>");
+			}
+		} catch (SQLException | IOException e) {
+			System.out.println("Exception Caught :" + e);
+			e.printStackTrace();
+		}
+
+	}
 }
