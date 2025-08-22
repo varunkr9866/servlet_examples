9d6473d79dd70fb8dd0b1f8508471b79ac0412de
varun <varunkr9866@gmail.com>
2025-08-22 05:36:15 +0530

    ALMOST COMPLETED

--- 12contextattributeregistrationapp/src/main/java/com/cluster/ShowAllDetailServlet.java
+++ 12contextattributeregistrationapp/src/main/java/com/cluster/ShowAllDetailServlet.java
@@ -1,6 +1,13 @@
 package com.cluster;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class ShowAllDetailServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
