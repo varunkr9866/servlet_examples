79a7f98e9b2c789c8f0033aab57200a4ef80a124
varun <varunkr9866@gmail.com>
2025-08-25 11:37:58 +0530

    CAUGHT AN EXCEPTION

--- 17sessionurlwriting/src/main/java/com/cluster/FirstServlet.java
+++ 17sessionurlwriting/src/main/java/com/cluster/FirstServlet.java
@@ -3,5 +3,23 @@ package com.cluster;
 import jakarta.servlet.http.HttpServlet;
 
 public class FirstServlet extends HttpServlet{
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		res.setContentType("text/html");
+		PrintWriter pw = res.getWriter();
+		String strEmail = req.getParameter("email");
 
+		HttpSession ses = req.getSession();
+		
+		pw.println("<html>");
+		pw.println("<body bgcolor='pink'>");
+		pw.println("<form>");
+		pw.println("<h1>");
+		pw.println("Email is " + strEmail + "<br></h1>");
+		pw.println("<a href="+res.encodeUrl("./html/abc.html")+">Click me for ABC </a><br>");
+		pw.println("<a href='./html/xyz.html'>Click me for XYZ</a><br>");
+		pw.println("</form>");
+		pw.println("</body>");
+		pw.println("</html>");
+	}
 }
